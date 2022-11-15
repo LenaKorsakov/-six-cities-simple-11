f9328116cf8741b5445820d5dc7ada10a4589acc
ElenaKorsakova <lenawwka@gmail.com>
2022-11-15 15:34:27 +0100

    Добавит компонент для городов без предложений

--- project/src/hooks/use-map.tsx
+++ project/src/hooks/use-map.tsx
@@ -2,10 +2,6 @@ import {useRef, useEffect, useState, MutableRefObject} from 'react';
 import {Map, TileLayer} from 'leaflet';
 import type {City} from '../../src/@types/offer-types';
 
-const tile = new TileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-});
-
 function useMap(mapRef: MutableRefObject<HTMLElement | null>, city: City): Map | null {
   const [map, setMap] = useState<Map | null>(null);
   const isRenderRef = useRef<boolean>(false);
@@ -20,6 +16,10 @@ function useMap(mapRef: MutableRefObject<HTMLElement | null>, city: City): Map |
       map.setView({lat, lng}, zoom, {animate: true});
       return;
     }
+
+    const tile = new TileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+    });
 
     const instance = new Map(mapRef.current, {
       zoom,
--- project/src/pages/main-screen/main-screen.tsx
+++ project/src/pages/main-screen/main-screen.tsx
@@ -4,6 +4,8 @@ import Header from '../../components/header/header';
 import OffersList from '../../components/offers-list/offers-list';
 import CitiesList from '../../components/cities-list/cities-list';
 import Map from '../../components/map/map';
+import Sort from '../../components/sort/sort';
+import OffersListEmpty from '../../components/offers-list-empty/offers-list-empty';
 
 import type {City, Offer} from '../../@types/offer-types';
 import { useAppSelector } from '../../hooks';
@@ -15,6 +17,7 @@ type MainScreenProps = {
 function MainScreen({cities}: MainScreenProps): JSX.Element {
   const selectedCity = useAppSelector((state) => state.city);
   const offers = useAppSelector((state)=> state.offers).filter((offer)=> offer.city.name === selectedCity.name);
+  const offersCount = offers.length;
 
   const [activeOffer, setActiveOffer] = useState<Offer | null>(null);
 
@@ -41,53 +44,28 @@ function MainScreen({cities}: MainScreenProps): JSX.Element {
           </section>
         </div>
         <div className="cities">
-          <div className="cities__places-container container">
-            <section className="cities__places places">
-              <h2 className="visually-hidden">Places</h2>
-              <b className="places__found"> {offers.length} places to stay in {selectedCity.name}</b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">
-                  Sort by
-                </span> {' '}
-                <span className="places__sorting-type" tabIndex={0}>
-                Popular
-                  <svg className="places__sorting-arrow" width={7} height={4}>
-                    <use xlinkHref="#icon-arrow-select" />
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li
-                    className="places__option places__option--active"
-                    tabIndex={0}
-                  >
-                  Popular
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                  Price: low to high
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                  Price: high to low
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                  Top rated first
-                  </li>
-                </ul>
-              </form>
-              <OffersList
-                onOfferHover={onOfferCardHover}
-                offers={offers}
-                isListMain
-              />
-            </section>
-            <div className="cities__right-section">
-              <Map
-                city={selectedCity}
-                offers={offers}
-                currentOffer={activeOffer}
-                isCityMap
-              />
-            </div>
-          </div>
+          {!offersCount ?
+            <OffersListEmpty/> :
+            <div className="cities__places-container container">
+              <section className="cities__places places">
+                <h2 className="visually-hidden">Places</h2>
+                <b className="places__found"> {offersCount} places to stay in {selectedCity.name}</b>
+                <Sort/>
+                <OffersList
+                  onOfferHover={onOfferCardHover}
+                  offers={offers}
+                  isListMain
+                />
+              </section>
+              <div className="cities__right-section">
+                <Map
+                  city={selectedCity}
+                  offers={offers}
+                  currentOffer={activeOffer}
+                  isCityMap
+                />
+              </div>
+            </div>}
         </div>
       </main>
     </div>
