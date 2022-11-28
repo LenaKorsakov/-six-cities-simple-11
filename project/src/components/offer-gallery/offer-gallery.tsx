bbabc41f9ee1d6ff8a99da26dec4b6ddc9f1ffb0
ElenaKorsakova <lenawwka@gmail.com>
2022-11-28 15:03:40 +0100

    Ограничит количество фотографий на странице

--- project/src/components/offer-gallery/offer-gallery.tsx
+++ project/src/components/offer-gallery/offer-gallery.tsx
@@ -4,13 +4,17 @@ type OfferGalleryProps = {
  offer: Offer;
 }
 
+const GALLERY_MAXLENGTH = 6;
+
 function OfferGallery({offer}: OfferGalleryProps): JSX.Element {
   const {images, title} = offer;
+
+  const visibleImages = images.slice(0, GALLERY_MAXLENGTH);
 
   return (
     <div className="property__gallery-container container">
       <div className="property__gallery">
-        {images && images.map((image, index) => {
+        {images && visibleImages.map((image, index) => {
           const keyValue = `${image}-${index}`;
           return (
             <div className="property__image-wrapper" key={keyValue}>
